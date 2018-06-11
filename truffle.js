46283f2ff9f26277373f3241a4aff17e6446915d
ShivaGuntuku <guntuku.shiva@hotmail.com>
2018-06-11 23:47:28 +0530

    modifications for network setup, contract update, migration warning resolved

--- truffle.js
+++ truffle.js
@@ -1,4 +1,13 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  // This file Used for Mac, Linux Users
+
+  networks: {
+    development: {
+        host: "127.0.0.1",
+        port: 7545,
+        network_id: "*" // for any network Id
+    }
+  }
 };
