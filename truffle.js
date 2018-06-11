9918d1f5c69e9c9ca8d859944333f4d83f371cb3
ShivaGuntuku <guntuku.shiva@hotmail.com>
2018-06-12 00:21:16 +0530

    under learning phase working improvement

--- truffle.js
+++ truffle.js
@@ -1,13 +1,11 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-  // This file Used for Mac, Linux Users
-
+  // for more about customizing your Truffle configuration!
   networks: {
     development: {
-        host: "127.0.0.1",
-        port: 7545,
-        network_id: "*" // for any network Id
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
     }
   }
 };
